59d4301c7ca2cc7b0f504220b35e0b802dbf7e86
jeffreyvanhelden <jeffrey.vanhelden@thewarehouse.co.nz>
2020-06-11 08:40:07 +1200

    IT test fixed

--- src/test/java/guru/bonacci/kafka/connect/tile38/Tile38SinkTaskIT.java
+++ src/test/java/guru/bonacci/kafka/connect/tile38/Tile38SinkTaskIT.java
@@ -15,9 +15,8 @@
  */
 package guru.bonacci.kafka.connect.tile38;
 
-import static io.lettuce.core.codec.StringCodec.UTF8;
-import static org.junit.Assert.assertNull;
 import static com.github.jcustenborder.kafka.connect.utils.SinkRecordHelper.write;
+import static io.lettuce.core.codec.StringCodec.UTF8;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
@@ -29,15 +28,21 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Stream;
 
+import org.apache.kafka.common.config.ConfigException;
 import org.apache.kafka.connect.data.Schema;
 import org.apache.kafka.connect.data.SchemaBuilder;
 import org.apache.kafka.connect.data.Struct;
+import org.apache.kafka.connect.errors.ConnectException;
 import org.apache.kafka.connect.sink.SinkRecord;
 import org.apache.kafka.connect.sink.SinkTask;
+import org.junit.Assert;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.testcontainers.containers.DockerComposeContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -50,7 +55,6 @@ import com.google.gson.JsonParser;
 import guru.bonacci.kafka.connect.tile38.config.Tile38SinkConnectorConfig;
 import io.lettuce.core.api.sync.RedisCommands;
 import io.lettuce.core.codec.StringCodec;
-import io.lettuce.core.output.BooleanOutput;
 import io.lettuce.core.output.StatusOutput;
 import io.lettuce.core.protocol.CommandArgs;
 import io.lettuce.core.protocol.CommandType;
@@ -93,6 +97,137 @@ public class Tile38SinkTaskIT {
 	}
 
 	@Test
+	public void emptyAssignment() {
+		final String topic = "foo";
+		Map<String, String> config = Maps.newHashMap(provideConfig(topic));
+		config.remove("tile38.topic.foo");
+
+		Assertions.assertThrows(ConfigException.class, () -> {
+			this.task.start(config);
+		});
+	}
+
+	@Test
+	public void invalidTopics() {
+		final String topic = "*#^$(^#$(&(#*$&($&(Q#";
+
+		Assertions.assertThrows(ConfigException.class, () -> {
+			this.task.start(provideConfig(topic));
+		});
+	}
+
+	@Test
+	public void topicRegex() {
+		final String topic = "foo";
+		Map<String, String> config = Maps.newHashMap(provideConfig(topic));
+		config.remove("topics");
+		config.put(SinkTask.TOPICS_REGEX_CONFIG, topic);
+		
+		Assertions.assertThrows(ConfigException.class, () -> {
+			this.task.start(config);
+		});
+	}
+
+	@Test
+	public void unconfiguredTopic() {
+		final String topic = "foo,bar";
+
+		Assertions.assertThrows(ConfigException.class, () -> {
+			this.task.start(provideConfig(topic));
+		});
+	}
+
+	@Test
+	public void emptyPut() {
+		final String topic = "foo";
+
+		this.task.start(provideConfig(topic));
+		this.task.put(ImmutableList.of());
+	}
+
+	static Stream<Arguments> provideForInvalidCommandTemplateStartup() {
+	    return Stream.of(
+	      Arguments.of("event.one event.two event.three"),
+	      Arguments.of(" set "),
+	      Arguments.of("set nokey")
+	    );
+	}
+
+	@ParameterizedTest
+	@MethodSource("provideForInvalidCommandTemplateStartup")
+  	public void invalidCommandTemplateStartup(String cmdString) {
+  	    final String topic = "foo";
+  		Map<String, String> config = Maps.newHashMap(provideConfig(topic));
+  		config.put("tile38.topic.foo", cmdString);
+
+  		Assertions.assertThrows(ConfigException.class, () -> {
+  	  		this.task.start(config);
+  		});
+  	}
+
+	private static Stream<Arguments> provideForValidWrite() {
+	    return Stream.of(
+	      Arguments.of("12.3", "4.56", "7.89"),
+	      Arguments.of("0.3", "1111.2", "6.9"),
+	      Arguments.of("15", "56", "89"),
+	      Arguments.of("0.3", "0", "0"),
+	      Arguments.of("1", "-0.0001", "-123.89"),
+	      Arguments.of("-12.3", "-4.56", "7.89")
+	    );
+	}
+	
+	@ParameterizedTest
+	@MethodSource("provideForValidWrite")
+	public void validWrites(String route, String lat, String lon) {
+	    final String topic = "foo";
+	    this.task.start(provideConfig(topic));
+
+		final String id = "fooid";
+		Schema schema = getRouteSchema();
+		Struct value = new Struct(schema).put("id", id).put("route", route).put("lat", lat).put("lon", lon);
+
+		final List<SinkRecord> records = ImmutableList.of(write(topic, Schema.STRING_SCHEMA, id, schema, value));
+		this.task.put(records);
+
+		RedisCommands<String, String> sync = this.task.getWriter().getClient().connect().sync();
+		CommandArgs<String, String> get = getFooCommand(id);
+		String resp = sync.dispatch(CommandType.GET, new StatusOutput<>(StringCodec.UTF8), get);
+		assertThat(parser.parse(resp), is(equalTo(parser.parse(String.format(RESULT_STRING, lon, lat)))));
+
+		resp = executeWithFieldsCommand(sync, get);
+		assertThat(resp, is(equalTo(route)));
+	}
+
+	@Test
+	public void nestedWrite() {
+		final String lat = "4.56", lon = "7.89";
+	    final String topic = "foo";
+
+		Map<String, String> config = Maps.newHashMap(provideConfig(topic));
+		config.put("tile38.topic.foo", "SET foo event.id POINT event.nested.lat event.nested.lon");
+		this.task.start(config);
+
+		final String id = "fooid";
+
+		Schema nestedSchema = SchemaBuilder.struct()
+				.field("lat", Schema.STRING_SCHEMA)
+				.field("lon", Schema.STRING_SCHEMA).build();
+		Schema schema = SchemaBuilder.struct().field("id", Schema.STRING_SCHEMA)
+				.field("nested", nestedSchema);
+
+		Struct nestedValue = new Struct(nestedSchema).put("lat", lat).put("lon", lon);
+		Struct value = new Struct(schema).put("id", id).put("nested", nestedValue);
+
+		final List<SinkRecord> records = ImmutableList.of(write(topic, Schema.STRING_SCHEMA, id, schema, value));
+		this.task.put(records);
+
+		RedisCommands<String, String> sync = this.task.getWriter().getClient().connect().sync();
+		CommandArgs<String, String> get = getFooCommand(id);
+		String resp = sync.dispatch(CommandType.GET, new StatusOutput<>(StringCodec.UTF8), get);
+		assertThat(parser.parse(resp), is(equalTo(parser.parse(String.format(RESULT_STRING, lon, lat)))));
+	}
+
+	@Test
 	public void expireMany() throws InterruptedException {
 		final String topic = "foo";
 	    this.task.start(provideConfig(topic));
@@ -114,12 +249,78 @@ public class Tile38SinkTaskIT {
 		for (int i=0; i<100; i++) {
 			CommandArgs<String, String> get = getFooCommand(id);
 			String resp = sync.dispatch(CommandType.GET, new StatusOutput<>(StringCodec.UTF8), get);
-			assertNull(resp);
+			Assert.assertNull(resp);
 		}	
 	}
-
-
 	
+	private static Stream<Arguments> provideForIgnoredFieldWrite() {
+	    return Stream.of(
+	      Arguments.of("0", "0.1", "1.0"),
+	      Arguments.of("0.0", "0.1", "1.0")
+	    );
+	}
+
+	@ParameterizedTest
+	@MethodSource("provideForIgnoredFieldWrite")
+	public void IgnoredFieldWrite(String route, String lat, String lon) {
+	    final String topic = "foo";
+		this.task.start(provideConfig(topic));
+
+		final String id = "fooid";
+		Schema schema = getRouteSchema();
+		Struct value = new Struct(schema).put("id", id).put("route", route).put("lat", lat).put("lon", lon);
+
+		final List<SinkRecord> records = ImmutableList.of(write(topic, Schema.STRING_SCHEMA, id, schema, value));
+		this.task.put(records);
+
+		RedisCommands<String, String> sync = this.task.getWriter().getClient().connect().sync();
+		CommandArgs<String, String> getCmd = getFooCommand(id);
+		String resp = executeWithFieldsCommand(sync, getCmd);
+
+		assertThat(resp, is(not(equalTo(route))));
+	}
+
+	private static Stream<Arguments> provideForInvalidWriteCommands() {
+	    return Stream.of(
+	      Arguments.of("SET foo event.one FIELD route event.two POINT event.three event.four", "fooid", "12.3", "string", "no float"),
+	      Arguments.of("SET foo  event.one FIELD route event.two POINT event.three event.four", "fooid", "null", "0.1", "1.0"),
+	      Arguments.of("SET foo event.one FIELD route event.two POINT event.three event.four", "fooid", "not a float", "3.1", "4.1"),
+	      Arguments.of("SET foo event.one FIELD route event.two POINT event.three event.four", "fooid", "nothing", "0.1", "1.0"),
+	      Arguments.of("SET foo event.one FIELD route event.two POINT event.three event.four", "fooid", "1f", "3.1", "4.1"),
+	      Arguments.of("SET foo event.one FIELD route event.two POINT event.three event.four", "fooid", "1", "3.1f", "4.1"),
+	      Arguments.of("SET foo event.one FIELD route event.two POINT event.three event.four", "fooid", "1F", "3.1", "4.1"),
+	      Arguments.of("SET foo event.one FIELD route event.two POINT event.three event.four", "fooid", "1", "3.1", "4.1F"),
+	      Arguments.of("SET foo event.one FIELD route event.two POINT event.three event.four", "fooid", "event.route", "event.lat", "event.lon"),
+  	      Arguments.of("SET bar event.one FIELD POINT event.two event.three", "123", "1.2", "2.3", null),
+	      Arguments.of("SET bar event.one FIELD POINT event.two event.three", "null", "%%", "@@", null),
+	      Arguments.of("SET bar event.one FIELD POINT event.two event.three", "$$", "1.2", "2.3", null),
+	      Arguments.of("SET bar event.one FIELD event.two event.three", "100", "1.2", "2.3", null)
+	    );
+	}
+	
+	@ParameterizedTest
+	@MethodSource("provideForInvalidWriteCommands")
+	public void invalidWriteCommands(String cmdString, String one, String two, String three, String four) {
+	    final String topic = "foo";
+		Map<String, String> config = Maps.newHashMap(provideConfig(topic));
+		config.put("tile38.topic.foo", cmdString);
+		this.task.start(config);
+
+		SchemaBuilder schemaBuilder = SchemaBuilder.struct().field("one", Schema.STRING_SCHEMA).field("two", Schema.STRING_SCHEMA)
+				.field("three", Schema.STRING_SCHEMA);
+		if (four != null)
+			schemaBuilder.field("four", Schema.STRING_SCHEMA);
+		Schema schema = schemaBuilder.build();
+
+		Struct valueBuilder = new Struct(schema).put("one", one).put("two", two).put("three", three);
+		Struct value = four != null ? valueBuilder.put("four", four) : valueBuilder;
+
+		final List<SinkRecord> records = ImmutableList.of(write(topic, Schema.STRING_SCHEMA, one, schema, value));
+		Assertions.assertThrows(ConnectException.class, () -> {
+			this.task.put(records);
+		});
+	}
+
 	// below some helper methods
 	private Map<String, String> provideConfig(String topic) {
 		return ImmutableMap.of(SinkTask.TOPICS_CONFIG, topic, 
